75b35a477a25bcf3e77330b71aee9a7b07350a91
Marius Fylling <marius@sdok.no>
2023-03-19 22:18:40 +0000

    fix default value for allowed hosts

--- tinbox/settings.py
+++ tinbox/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
 
 # Application definition
